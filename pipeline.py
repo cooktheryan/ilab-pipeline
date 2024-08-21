f4a673fc671062c3ca61360bac33189144985892
Ryan Cook <rcook@redhat.com>
2024-08-21 15:42:42 -0400

    rewrite
    Signed-off-by: Ryan Cook <rcook@redhat.com>

--- pipeline.py
+++ pipeline.py
@@ -7,12 +7,14 @@ from components import ilab_init_op, ilab_model_op, ilab_taxonomy_fork_clone_op,
     description='Instructlab training workflow'
 )
 def ilab_pipeline():
-    ilab_init = ilab_init_op()
-    ilab_model = ilab_model_op().after(ilab_init)
-    ilab_taxonomy_fork_clone = ilab_taxonomy_fork_clone_op().after(ilab_model)
-    ilab_taxonomy_sed = ilab_taxonomy_sed_op().after(ilab_taxonomy_fork_clone)
-    ilab_taxonomy_validate = ilab_taxonomy_validate_op().after(ilab_taxonomy_sed)
-    ilab_train = ilab_train_op().after(ilab_model, ilab_taxonomy_validate)
+    taxonomy_volume = dsl.Volume(name='taxonomy', pvc='taxonomy')
+
+    ilab_init = ilab_init_op().add_volume(taxonomy_volume)
+    ilab_model = ilab_model_op().after(ilab_init).add_volume(taxonomy_volume)
+    ilab_taxonomy_fork_clone = ilab_taxonomy_fork_clone_op().after(ilab_model).add_volume(taxonomy_volume)
+    ilab_taxonomy_sed = ilab_taxonomy_sed_op().after(ilab_taxonomy_fork_clone).add_volume(taxonomy_volume)
+    ilab_taxonomy_validate = ilab_taxonomy_validate_op().after(ilab_taxonomy_sed).add_volume(taxonomy_volume)
+    ilab_train = ilab_train_op().after(ilab_model, ilab_taxonomy_validate).add_volume(taxonomy_volume)
 
 if __name__ == '__main__':
     kfp.compiler.Compiler().compile(ilab_pipeline, 'ilab_pipeline.yaml')
